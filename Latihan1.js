fbf6e58d46aa01c191feaa690d24e26f84742fe0
ihwenwen20 <ihwannh96@gmail.com>
2023-03-30 05:13:33 +0700

    first commit

--- Latihan1.js
+++ Latihan1.js
@@ -2,30 +2,29 @@
 // Inisialisasi variabel untuk menghitung jumlah item
 let count = 0;
 
-const addForm = document.getElementById("addForm");
-const todoInput = document.getElementById("todoInput");
 const todoList = document.getElementById("todoList");
+const todoInput = document.getElementById("todoInput");
 const errorMsg = document.querySelector('.required');
 
 // Fungsi untuk menambahkan item ke dalam daftar
 function addItem() {
-    count++;
-    const todo = todoInput.value;
-    const row = `
+  count++;
+  const todo = todoInput.value.trim();
+  const row = `
   <tr>
     <th scope="row">${count}</th>
     <td>${todo}</td>
     <td><button class="btn btn-danger btn-sm" onclick="deleteItem(this)">Delete</button></td>
   </tr>
 `;
-    todoList.insertAdjacentHTML("beforeend", row);
-    todoInput.value = "";
+  todoList.insertAdjacentHTML("beforeend", row);
+  todoInput.value = "";
 }
 
 // Fungsi untuk menghapus item dari daftar
 function deleteItem(btn) {
-    const row = btn.parentNode.parentNode;
-    row.parentNode.removeChild(row);
+  const row = btn.parentNode.parentNode;
+  row.parentNode.removeChild(row);
 }
 
 // Fungsi untuk validasi input todo
@@ -34,12 +33,16 @@ function validateTodoInput() {
   if (todo === "") {
     errorMsg.innerText = "Todo nya masih kosong, ya!";
     return false;
+  } else if ([...todoList.children].find(item => item.children[1].innerText === todo)) {
+    errorMsg.innerText = "Owalah, Todo sudah ada ternyata!";
+    return false;
   }
   return true;
 }
 
 // Event listener untuk menambahkan item ke dalam daftar
-addForm.addEventListener('submit', function(event) {
+const addForm = document.getElementById("addForm");
+addForm.addEventListener('submit', function (event) {
   event.preventDefault();
   if (validateTodoInput()) {
     addItem();
